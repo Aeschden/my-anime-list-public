4a5a8fdfccdd2eaed6499bea5f092a7040cb64ef
Aeschden <aeschden@students.zhaw.ch>
2025-05-31 11:00:58 +0200

    c

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  build: {
-    rollupOptions: {
-      // Prevent Rollup from trying to bundle `mongodb` into your client output
-      external: ['mongodb']
-    }
-  }
+  plugins: [sveltekit()]
 });
